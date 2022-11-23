d52056835b96dda6ce733fed970c89e8cf8ef57c
dimadd <dima.dragos0@gmail.com>
2022-11-23 12:17:01 +0200

    Created functions to get and post a User

--- lib/api/remote_services.dart
+++ lib/api/remote_services.dart
@@ -1,31 +1,30 @@
-import 'student.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:city_dangers_alert/api/user.dart';
 const String baseUrl = 'http://10.0.2.2:5000/students';
 class RemoteServices
 {
-  Future<List<Student>?> getStudents(String id) async
-  {
+
+  Future<UserData?> getUser(String id) async {
     var client = http.Client();
 
-    var uri = Uri.parse('http://10.0.2.2:5000/studentid/' + id);
+    var uri = Uri.parse('http://10.0.2.2:5000/user/' + id);
     var response = await client.get(uri);
-    if(response.statusCode == 200)
-    {
+    if (response.statusCode == 200){
       var json = response.body;
-      return studentFromJson(json);
+      return userFromJson(json);
     }
   }
 
-  Future<dynamic> postStudent(dynamic object) async {
-    var url = Uri.parse(baseUrl);
+  Future<dynamic> postUser(dynamic object) async {
+    var url = Uri.parse('http://10.0.2.2:5000/adduser');
     var client = http.Client();
     var _payload = json.encode(object);
     var _headers = {
       'Content-Type':'application/json',
     };
 
-    var response = await client.post(url, body: _payload, headers: _headers);
-    var json1 = response.body;
+    await client.post(url, body: _payload, headers: _headers);
   }
+
 }
